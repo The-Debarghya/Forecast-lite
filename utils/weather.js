a99b7858efdbcf1ad56e2b97bd636817d21a6c6b
Debarghya Maitra <debarghya4444@gmail.com>
2021-12-23 16:37:13 +0530

    Update weather.js

--- utils/weather.js
+++ utils/weather.js
@@ -4,13 +4,13 @@ module.exports = async (location) =>{
 
   let settings = { method: "Get" };
   if (typeof location == 'object') {
-    let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${location[0]}&lon=${location[1]}&exclude=minutely,hourly&units=metric&APPID=aade92db2619aca3f567b1ac542128b5`;
+    let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${location[0]}&lon=${location[1]}&exclude=minutely,hourly&units=metric&APPID=apikey`;
     const response = await fetch(url, settings)
     const data = await response.json();
     console.log(`\nCurrent conditions in ${data['timezone']} is:`)
     return data
   } else {
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&APPID=aade92db2619aca3f567b1ac542128b5`;
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&APPID=apikey`;
     const response = await fetch(url, settings)
     const data = await response.json();
     console.log(`\nCurrent conditions in ${location} is:`)
